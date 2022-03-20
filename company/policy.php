5af374fc1c06d9f6ba003590f46c6dab079d83f5
YukiarC <yuukihagino@gmail.com>
2022-03-20 21:56:51 +0900

    feat: added info of company.

---
+++ company/policy.php
@@ -0,0 +1,91 @@
+<?php
+$tit = "プライバシーポリシー｜カードローン情報局";
+$desc = "カードローンの情報を扱うカードローン情報局。";
+?>
+<?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/header.php'); ?>
+<div class="row">
+    <div class="col-12">
+        <h2 class="top">プライバシーポリシー</h2>
+        <p>当サイトでは、ユーザーの皆様から取得する個人情報につきまして、以下の個人情報の保護に関する基本方針に基づいて、適切に取り扱い、その保護に努めます。</p>
+        <h3>個人情報の定義</h3>
+        <p>「個人情報」とは、次のような生存する個人を特定、識別できる情報をいいます。</p>
+        <p>１、お名前、ご住所、生年月日、性別、ご職業、お電話番号、電子メールアドレス等</p>
+        <p>２、その情報のみでは特定の個人を識別できないが、他の情報と容易に照合することができ、この照合により個人を特定できる情報</p>
+        <h3>個人情報の取得</h3>
+        <p>１、当サイトではサイトの運営活動以外の目的で個人情報を取得することはありません。</p>
+        <p>２、当サイトは虚偽、その他不正な手段を用いて個人情報を取得することはありません。</p>
+        <p>３、当サイトがサイト利用者から個人情報を取得するときは、その利用目的を明らかにするものとします。</p>
+        <h3>個人情報の第三者への提供</h3>
+        <p>当サイトでは下記のいずれかに該当する場合を除き、サイト利用者の個人情報を第三者に提供することはありません。また、下記の事由により個人情報の提供を行う場合は、保有する個人情報のうち必要最小限の項目のみを提供するものとします。
+        </p>
+        <p>１、お客さまから同意をいただいた場合</p>
+        <p>２、国の機関や地方公共団体が法令で定める事務を遂行することに対して協力が必要な場合</p>
+        <p>３、その他法令に基づく場合</p>
+        <h3>関係法令の遵守</h3>
+        <p>当サイトでは個人情報の取り扱いに関係して日本国の法令を遵守いたします。</p>
+        <p>制定：2022年3月1日 | 株式会社〇〇</p>
+    </div>
+    <div class="col-12">
+        <h2>WEBサービスでの情報収集について</h2>
+        <p>当社が運営するウェブサイトの閲覧データから、アクセス情報の収集のためにクッキー(Cookie)やWEBビーコンを使用しています。</p>
+        <p>そこで得られたデータは、閲覧状況の分析、当社が運営するウェブサイト間における連携、当社が提携している第三者企業サービスとの連携、アンケート調査、広告表示・広告効果の測定などの用途に使用します。これらは、より良いサービスをご提供する目的で使用するものであり、これによって、お客様個人を特定できるものではありません。
+        </p>
+        <h3>Cookieについて</h3>
+        <p>Cookieとは、ウェブページを利用したときに、ブラウザとサーバーとの間で送受信した利用履歴や入力内容などをお客様のコンピュータに一時的にデータを書き込んで保存させる仕組みです。</p>
+        <h3>クッキー機能を停止するには</h3>
+        <p>ブラウザの設定により、クッキーの受け取りを拒否したり、クッキーを受け取ったとき警告を表示させたりできます。設定の仕方は、各ブラウザの「ヘルプ」メニューにある「クッキー」や「Cookie」などの項目をご確認ください。
+        </p>
+        <h3>計測ツール</h3>
+        <p>アクセスデータ収集・解析には、Google Analytics、Treasure Dataを主に使用しています。Google
+            Analyticsによって収集されるアクセスデータは、Google社のプライバシーポリシーに基づいて管理されます。Google
+            Analyticsについて、およびGoogle社のプライバシーポリシーについては「関連情報」をご覧ください。</p>
+        <h3>収集されるアクセスデータ例</h3>
+        <ul>
+            <li>・当社が運営するウェブサイトを閲覧した日時、閲覧したURL</li>
+            <li>・コンピュータ端末のIPアドレス</li>
+            <li>・オペレーティングシステム、ブラウザの種類</li>
+            <li>・リファラー情報（利用者がどのウェブサイトから当社が運営するウェブサイトに訪問したのかという情報です）</li>
+        </ul>
+        <p>関連情報<br>Googleプライバシーポリシー<br><a href="https://www.google.com/intl/ja/policies/privacy/"
+                target="_blank">https://www.google.com/intl/ja/policies/privacy/</a></p>
+        <h3>広告</h3>
+        <p>当社が運営するサイトに表示されている広告の一部は第三者企業に配信を委託しています。第三者企業もクッキーやWEBビーコンを利用する場合があります。
+            また、当社が運営するサイトは、利用者への適切な広告配信を目的に以下の行動ターゲティング広告を利用していますが、このサービスは無効にすることができます。 </p>
+        <p>サービスの無効化を希望される場合は、以下の各社のサイトにて手続きをお願いいたします。</p>
+        <ul>
+            <li>・グーグル合同会社 <br><a href="https://www.google.com/intl/ja/policies/technologies/ads/"
+                    target="_blank">https://www.google.com/intl/ja/policies/technologies/ads/</a></li>
+            <li>・株式会社サイバー・コミュニケーションズ<br><a href="http://www.beyondx.jp/optout.html"
+                    target="_blank">http://www.beyondx.jp/optout.html</a></li>
+            <li>・株式会社マイクロアド <br><a href="http://send.microad.jp/w3c/" target="_blank">http://send.microad.jp/w3c/</a>
+            </li>
+            <li>・GMOアドパートナーズ株式会社<br><a href="http://www.gmo-ap.jp/privacy.html"
+                    target="_blank">http://www.gmo-ap.jp/privacy.html</a></li>
+            <li>・株式会社プラットフォーム・ワン<br><a href="https://www.platform-one.co.jp/optout.html"
+                    target="_blank">https://www.platform-one.co.jp/optout.html</a></li>
+            <li>・スマートニュース株式会社 <br><a href="http://www.smartnews-ads.com/optout/"
+                    target="_blank">http://www.smartnews-ads.com/optout/</a></li>
+            <li>・Facebook Japan株式会社<br><a href="https://www.facebook.com/privacy/explanation"
+                    target="_blank">https://www.facebook.com/privacy/explanation</a></li>
+            <li>・Oath Japan株式会社<br><a href="https://www.aolplatforms.jp/company/privacy"
+                    target="_blank">https://www.aolplatforms.jp/company/privacy</a></li>
+            <li>・株式会社Gunosy<br><a href="https://gunosy.com/about/network_targeting_ads"
+                    target="_blank">https://gunosy.com/about/network_targeting_ads</a></li>
+            <li>・Criteo株式会社<br><a href="https://www.criteo.com/jp/privacy/"
+                    target="_blank">https://www.criteo.com/jp/privacy/</a></li>
+            <li>・ヤフー株式会社</li>
+            <ul style="margin:0;">
+                <li>プライバシーポリシー<br><a href="https://about.yahoo.co.jp/docs/info/terms/chapter1.html#cf2nd"
+                        target="_blank">https://about.yahoo.co.jp/docs/info/terms/chapter1.html#cf2nd</a></li>
+                <li>行動ターゲティング広告オプトアウトページ<br><a href="https://btoptout.yahoo.co.jp/optout/index.html"
+                        target="_blank">https://btoptout.yahoo.co.jp/</a></li>
+            </ul>
+        </ul>
+        <h3>SSL (Secure Sockets Layer)/TLS (Transport Layer Security)について</h3>
+        <p>弊社は、弊社サイトを通じてご本人から個人情報をご提供いただく際は、通信途上における第三者の盗聴等を防止するため、SSL (Secure Sockets Layer)/TLS (Transport Layer
+            Security)による暗号化またはこれに準ずるセキュリティ技術を使用します。</p>
+        <p>ただし、ご本人のコンピューター環境により、まれにこれらの技術をご利用いただけない場合がございます。その場合には、通信途上における安全性が確保できない旨の表示をいたします。</p>
+        <p>制定：2022年3月1日 | 株式会社〇〇</p>
+    </div>
+</div>
+<?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/footer2.php'); ?>
