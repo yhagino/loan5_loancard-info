6bb2e96f7b848969d24c952eeb940621e9dfb4bd
YukiarC <yuukihagino@gmail.com>
2022-03-21 15:32:15 +0900

    feat: added item.php for sp.

--- search/index.php
+++ search/index.php
@@ -4,31 +4,38 @@ $desc = "カードローンの情報を扱うカードローン情報局。";
 include_once( $_SERVER['DOCUMENT_ROOT'] . '/search/result.php');
 ?>
 <?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/header.php'); ?>
-<div class="row mt-10 mb-20">
-    <form class="col-12" name="search" action="https://cardloan-info.net/search/index.php" method="post"
-        accept-charset="utf-8">
-        <input type="hidden" value="<?php echo $_POST['limit_id'];?>" name="limit_id">
-        <input type="hidden" value="<?php echo $_POST['examination_id'];?>" name="examination_id">
-        <input type="hidden" value="<?php echo $_POST['loantime_id'];?>" name="loantime_id">
-        <input type="hidden" value="<?php echo $_POST['convenience_id'];?>" name="convenience_id">
-        <div class="input-group col-12 offset-md-6 col-md-6">
-            <select class="custom-select" id="FormSelect1" name="sort">
-                <option selected>Choose...</option>
-                <option value="rate_min">最低利率順</option>
-                <option value="rate_max">最高利率順</option>
-                <option value="name">名前順</option>
-            </select>
-            <div class="input-group-append">
-                <button class="btn btn-outline-secondary" type="submit" name="sort_button">並び替え</button>
+<div class="container-fluid">
+    <div class="row mt-10 mb-20">
+        <form class="col-12" name="search" action="https://cardloan-info.net/search/index.php" method="post"
+            accept-charset="utf-8">
+            <input type="hidden" value="<?php echo $_POST['limit_id'];?>" name="limit_id">
+            <input type="hidden" value="<?php echo $_POST['examination_id'];?>" name="examination_id">
+            <input type="hidden" value="<?php echo $_POST['loantime_id'];?>" name="loantime_id">
+            <input type="hidden" value="<?php echo $_POST['convenience_id'];?>" name="convenience_id">
+            <div class="input-group col-12 offset-md-6 col-md-6">
+                <select class="custom-select" id="FormSelect1" name="sort">
+                    <option selected>Choose...</option>
+                    <option value="rate_min">最低利率順</option>
+                    <option value="rate_max">最高利率順</option>
+                    <option value="name">名前順</option>
+                </select>
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="submit" name="sort_button">並び替え</button>
+                </div>
             </div>
-        </div>
-    </form>
-</div>
-<!-- form end -->
-<div class="row">
-    <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)){ ?>
-    <?php include( $_SERVER['DOCUMENT_ROOT'] . '/common/item.php'); ?>
-    <?php }; ?>
-</div>
-</main>
-<?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/footer2.php'); ?>
+        </form>
+    </div>
+    <!-- form end -->
+    <div class="row">
+        <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)){ ?>
+        <?php
+        $ua = $_SERVER['HTTP_USER_AGENT'];
+        if ((strpos($ua, 'Android') !== false) && (strpos($ua, 'Mobile') !== false) || (strpos($ua, 'iPhone') !== false) || (strpos($ua, 'Windows Phone') !== false)) : ?>
+        <?php include( $_SERVER['DOCUMENT_ROOT'] . '/common/item_sp.php'); ?>
+        <?php else:?>
+        <?php include( $_SERVER['DOCUMENT_ROOT'] . '/common/item_pc.php'); ?>
+        <?php endif;?>
+        <?php }; ?>
+    </div>
+    </main>
+    <?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/footer2.php'); ?>
