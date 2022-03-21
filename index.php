d919e58992c20bec1c09b35e965c377aedf9ab56
YukiarC <yuukihagino@gmail.com>
2022-03-21 15:33:01 +0900

    docs: changed somewhat.

--- index.php
+++ index.php
@@ -3,19 +3,23 @@ $tit = "カードローン情報局";
 $desc = "カードローンの情報を扱うカードローン情報局。";
 ?>
 <?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/header.php'); ?>
-<div class="row">
-    <div class="col-md-9 col-12">
-        <main>
-            <div class="row mt-80">
-                <div class="col-12">
-                    <h2 class="mb-30">どうしても審査に通過したい方におすすめのカードローン</h2>
-                    <p>WEB完結なら電話連絡なし、郵送物不要でも可能。最短30分審査で即日借入可能なカードローンです。</p>
+<div class="top-img">
+    <img class="img-fluid" src="/img/top.png" alt="">
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-9 col-12">
+            <main>
+                <div class="row mt-80">
+                    <div class="col-12">
+                        <h2 class="mb-30">どうしても審査に通過したい方におすすめのカードローン</h2>
+                        <p>WEB完結なら電話連絡なし、郵送物不要でも可能。最短30分審査で即日借入可能なカードローンです。</p>
+                    </div>
                 </div>
-            </div>
-            <div class="row mt-80">
-                <div class="col-12">
-                    <?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/input.php');?>
+                <div class="row mt-80">
+                    <div class="col-12">
+                        <?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/input.php');?>
+                    </div>
                 </div>
-            </div>
-        </main>
-        <?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php'); ?>
+            </main>
+            <?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/footer.php'); ?>
