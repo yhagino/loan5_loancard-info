de61e82f91bb2a9eab1b4a3a3af4c2f881f8f4fa
YukiarC <yuukihagino@gmail.com>
2022-06-01 22:00:41 +0900

    docs: changed url and added cnt.

---
+++ cnt/loan-sokujitu.php
@@ -0,0 +1,25 @@
+<?php
+$tit = "即日融資|カードローン情報局";
+$desc = "カードローンの情報を扱うカードローン情報局。";
+?>
+<?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/header.php'); ?>
+<div class="top-img">
+    <img class="img-fluid" src="/img/top.png" alt="">
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 col-12">
+            <main>
+                <div class="row mt-80">
+                    <div class="col-12">
+                        <h2 class="mb-30">最短30分! 審査スピード重視ならこの会社</h2>
+                        <p>即日審査・即日融資可能なカードローン</p>
+                    </div>
+                </div>
+                <div class="row mt-80">
+                    <div class="col-12">
+                        <?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/input.php');?>
+                    </div>
+                </div>
+            </main>
+            <?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/footer2.php'); ?>
---
+++ cnt/loan-zaiseki.php
@@ -0,0 +1,24 @@
+<?php
+$tit = "在籍確認なし|カードローン情報局";
+$desc = "カードローンの情報を扱うカードローン情報局。";
+?>
+<?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/header.php'); ?>
+<div class="top-img">
+    <img class="img-fluid" src="/img/top.png" alt="">
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 col-12">
+            <main>
+                <div class="row mt-80">
+                    <div class="col-12">
+                        <h2 class="mb-30">秘密でお金を借りたい人必見</h2>
+                    </div>
+                </div>
+                <div class="row mt-80">
+                    <div class="col-12">
+                        <?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/input.php');?>
+                    </div>
+                </div>
+            </main>
+            <?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/common/footer2.php'); ?>
